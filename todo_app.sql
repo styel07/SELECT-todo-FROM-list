c3123cf36a335abb4ec85e27c30574d2e71d2e8e
styel07 <eleazarcorpuz@gmail.com>
2015-10-22 13:48:23 -1000

    created 2 new tasks and added a NOW() to created_at table definition

--- todo_app.sql
+++ todo_app.sql
@@ -36,14 +36,19 @@ DROP completed;
 
 -- add a column to tasks named completed_at:timestamp, that may be NULL, and has a default value of NULL.
 ALTER TABLE tasks
-ADD completed_at timestamp DEFAULT NULL;
+ADD completed_at timestamp
+DEFAULT NULL;
 
 -- change the updated_at column to not allow NULL values,
 ALTER TABLE tasks
 ALTER COLUMN updated_at
 SET NOT NULL;
 
---and have a default value of now()
+-- created_at and updated_at have a default value of now()
+ALTER TABLE tasks
+ALTER COLUMN created_at
+SET DEFAULT NOW();
+
 ALTER TABLE tasks
 ALTER COLUMN updated_at
 SET DEFAULT NOW();
@@ -53,19 +58,42 @@ INSERT INTO tasks (title, description, created_at)
 VALUES ('Study SQL', 'Complete this exercise', NOW());
 
 -- create a new task title = 'Study PostgreSQL' description = 'Read all the documentation'
+INSERT INTO tasks (title, description)
+VALUES ('Study PostgreSQL', 'Read all the documentation');
 
 -- select all the titles of tasks that are not yet completed
+SELECT title
+FROM tasks
+WHERE completed_at IS NULL;
 
 -- update the task with a title of 'Study SQL' to be completed as of now
 
+UPDATE tasks
+SET completed_at = NOW()
+WHERE title = 'Study SQL';
+
 -- select all titles and descriptions of tasks that are not yet completed
+SELECT title, description
+FROM tasks
+WHERE completed_at IS NULL;
 
 -- select all fields of every task sorted by creation date in descending order
+SELECT *
+FROM tasks
+ORDER BY created_at DESC;
+
 -- create a new task title = 'mistake 1' description = 'a test entry'
+
 -- create a new task title = 'mistake 2' description = 'another test entry'
+
 -- create a new task title = 'third mistake' description = 'another test entry'
+
 -- select title fields of all tasks with a title that includes the word 'mistake'
+
 -- delete the task that has a title of mistake 1
+
 -- select title and description fields of all tasks with a title that includes the word 'mistake'
+
 -- delete all tasks that includes the word 'mistake' in the title
+
 -- select all fields of all tasks sorted by title in ascending order
